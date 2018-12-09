104fb92b7f837933a6323c0f77c060c5732b2233
Александр Герасименко <a.gerasimenko@elonsoft.ru>
2018-12-09 11:53:51 +0300

    Add moneyFlows controllers

---
+++ app/controllers/money_flows_controller.rb
@@ -0,0 +1,27 @@
+class MoneyFlowsController < ApplicationController
+  def index; end
+
+  def create
+    if MoneyFlow.create(money_flow_params)
+      render json: {}, status: :ok
+    end
+  end
+
+  def update
+    if MoneyFlow.find(params[:id]).update(money_flow_params)
+      render json: {}, status: :ok
+    end
+  end
+
+  def destroy
+    if MoneyFlow.find(params[:id]).destroy
+      render json: {}, status: :ok
+    end
+  end
+
+  private
+
+  def money_flow_params
+    params.require(:money_flow).permit(:description, :amount, :kind, :day_id)
+  end
+end
---
+++ app/controllers/period_money_flows_controller.rb
@@ -0,0 +1,27 @@
+class PeriodMoneyFlowsController < ApplicationController
+  def index; end
+
+  def create
+    if PeriodMoneyFlow.create(money_flow_params)
+      render json: {}, status: :ok
+    end
+  end
+
+  def update
+    if PeriodMoneyFlow.find(params[:id]).update(money_flow_params)
+      render json: {}, status: :ok
+    end
+  end
+
+  def destroy
+    if PeriodMoneyFlow.find(params[:id]).destroy
+      render json: {}, status: :ok
+    end
+  end
+
+  private
+
+  def money_flow_params
+    params.require(:money_flow).permit(:description, :amount, :kind, :period_id)
+  end
+end
