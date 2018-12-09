479bb491ea48b6657cb2e1b8cdd5cfa5d6e2d48f
Александр Герасименко <a.gerasimenko@elonsoft.ru>
2018-12-09 11:55:03 +0300

    Update PeriodMoneyFlow

--- app/models/period_money_flow.rb
+++ app/models/period_money_flow.rb
@@ -1,3 +1,11 @@
 class PeriodMoneyFlow < ApplicationRecord
+  # === Relations
   belongs_to :period
+
+  # === Enums
+  enum kind: [:income, :expense]
+
+  # === Validations
+  validates :kind, presence: true
+  validates :amount, presence: true
 end
